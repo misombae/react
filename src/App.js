2c4f4806d33a9221a4bcfc6d15371ae1563ce8f1
misombae <jung00351@daum.net>
2021-01-10 01:06:23 +0900

    state list, key사용

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ class App extends Component {
   constructor(props) { //componet실행전 props초기화
     super(props);
     this.state = { //state 초기화 외부에 필요없는 정보는 state에 은닉한다.
-      subject:{title:'WEB', sub:'World Wid Web'}
+      subject:{title: 'WEB', sub: 'World Wid Web'},
+      contents:[
+        {id:1, title: 'HTML', desc: 'HTML is HyperText Markup Language.'},
+        {id:2, title: 'CSS', des: 'CSS is for design'},
+        {id:3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
+      ]
     }
   }
 
@@ -17,7 +22,7 @@ class App extends Component {
     return (
       <div className="App">
           <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-          <TOC></TOC>
+          <TOC data = {this.state.contents}></TOC>
           <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
--- src/component/TOC.js
+++ src/component/TOC.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
     render() {
+      var list = [];
+      var data = this.props.data;
+      var i = 0;
+      while(i < data.length){
+        list.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        i = i + 1;
+      }
       return (
         <nav>
           <ul>
-              <li><a href="1.html">HTML</a></li>
-              <li><a href="2.html">CS</a></li>
-              <li><a href="3.html">JavaScript</a></li>
+             {list}
           </ul>
         </nav>
       );  
